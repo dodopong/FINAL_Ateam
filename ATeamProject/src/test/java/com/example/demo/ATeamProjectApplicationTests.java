e7e9c422ec09408fde3a3bbc135a15cbfa220ed8
beakgyeoungbin <qsxza15@naver.com>
2024-09-12 14:07:10 +0900

    20240912 first commit

--- ATeamProject/src/test/java/com/example/demo/ATeamProjectApplicationTests.java
+++ ATeamProject/src/test/java/com/example/demo/ATeamProjectApplicationTests.java
@@ -14,11 +14,7 @@ class ATeamProjectApplicationTests {
 
 	    @Test
 	    void testJpa() {
-	        for (int i = 1; i <= 300; i++) {
-	            String subject = String.format("테스트 데이터입니다:[%03d]", i);
-	            String content = "내용무";
-	            this.boardQuestionService.boardQuestionCreate(subject, content);
-	        }
+
 	    }
 
 }
